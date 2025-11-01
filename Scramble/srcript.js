32a563a882a05be7476eb7006194918f5c4a331f
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-01 23:13:55 +0530

    add

--- Scramble/srcript.js
+++ Scramble/srcript.js
@@ -1,11 +1,16 @@
-gsap.to("p", {
-    duration:3,
-    scrambleText:{
-        text:"GSAP Scramble text",
-        chars:"XO",
-        revealDelay:0.3,
-        speed:1,
-        tweenLength:false
-        ,newClass:"text-color"
-    }
+// gsap.to("p", {
+//     duration:3,
+//     scrambleText:{
+//         text:"GSAP Scramble text",
+//         chars:"XO",
+//         revealDelay:0.3,
+//         speed:1,
+//         tweenLength:false
+//         ,newClass:"text-color"
+//     }
+// })
+
+const p = document.querySelector("p")
+p.addEventListener("mouseenter",() => {
+ 
 })
