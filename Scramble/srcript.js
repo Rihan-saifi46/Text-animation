2c8ebc5bf875ebc9d708c59518418d2f93bc9bdc
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-01 23:14:22 +0530

    add

--- Scramble/srcript.js
+++ Scramble/srcript.js
@@ -12,5 +12,15 @@
 
 const p = document.querySelector("p")
 p.addEventListener("mouseenter",() => {
- 
+ gsap.to("p", {
+    duration:3,
+    scrambleText:{
+        text:"GSAP Scramble text",
+        chars:"XO",
+        revealDelay:0.3,
+        speed:1,
+        tweenLength:false
+        ,newClass:"text-color"
+    }
+})
 })
