dd1fa5fe16631bef3b482bc6bbea1d89ca2661ca
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-02 15:41:13 +0530

    add

--- Reveal/src.js
+++ Reveal/src.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(SplitText)
 
-    let split = SplitText.create(".reveal-text")
+    let split = SplitText.create("p",{
+        type:"lines,words,chars"
+    })
+
+    gsap.from(split.chars,{
+        y:30,
+        opacity:0,
+        stagger:{
+            each:0.1,
+            from:"end"
+        }
+    })
 })
