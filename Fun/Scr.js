01a27e904f80907d02fcc05b0ae56a99227930e4
Rihan-saifi46 <kharihan786k@gmail.com>
2025-11-12 10:22:33 +0530

    aadd

--- Fun/Scr.js
+++ Fun/Scr.js
@@ -1,11 +1,23 @@
 gsap.registerPlugin(SplitText)
 
 let split = SplitText.create(".text",{
-  type:"words , chars , lines"
+  type:"words , chars , lines",
+  wordsClass:"word"
 })
 
-gsap.from(split.words,{
-    y:100,
+gsap.from(split.chars,{
+    // yPercent:"random([-150,150])",
+    y:"random([-150,150])",
+
+    // rotation:"random([-45,45])",
     autoAlpha:0,
-    stagger:0.05
+    //   repeat:-1,
+    //     yoyo:true,
+        ease:"back.out",
+    stagger:{
+        amount:".5",
+        from:"random",
+        repeat:-1,
+        yoyo:true
+    }
 })
